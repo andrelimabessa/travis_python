fec205ff25132949b00fad1bbcdf171f21c90155
andre.bessa <andre.bessa@dataprev.gov.br>
2019-10-11 19:52:08 -0300

    Add projeto

---
+++ string_func/__init__.py
@@ -0,0 +1,4 @@
+
+from .stringLength import stringLength
+from .stringToLower import stringToLower
+from .stringToUpper import stringToUpper
---
+++ test/test_sample.py
@@ -0,0 +1,4 @@
+from .context import string_func
+
+def test_stringLength():
+    assert string_func.stringLength("andre") == 5 
