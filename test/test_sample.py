b9408380e7eb0551bd5076c7f028f19cae2d45a7
andrelimabessa <andrebessa@gmail.com>
2019-10-12 02:11:18 -0300

    sdfsdfsd
    sdfsdfsdfdf

--- test/test_sample.py
+++ test/test_sample.py
@@ -2,3 +2,9 @@ from .context import string_func
 
 def test_stringLength():
     assert string_func.stringLength("andre") == 5 
+
+def test_stringToLower():
+    assert string_func.stringToLower("Andre") == 'andre'
+
+def test_stringToUpper():
+    assert string_func.stringToUpper("Andre") == 'ANDRE'
